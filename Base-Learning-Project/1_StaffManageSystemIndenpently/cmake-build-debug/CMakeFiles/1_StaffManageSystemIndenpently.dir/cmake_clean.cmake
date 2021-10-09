10c199ccec222f3d29ac31246ce96f10a7bc2aab
Henry <me@ldylab.cc>
2021-10-09 21:44:16 +0800

    Edit the project

---
+++ Base-Learning-Project/1_StaffManageSystemIndenpently/cmake-build-debug/CMakeFiles/1_StaffManageSystemIndenpently.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "1_StaffManageSystemIndenpently"
+  "1_StaffManageSystemIndenpently.pdb"
+  "CMakeFiles/1_StaffManageSystemIndenpently.dir/boss.cpp.o"
+  "CMakeFiles/1_StaffManageSystemIndenpently.dir/employee.cpp.o"
+  "CMakeFiles/1_StaffManageSystemIndenpently.dir/main.cpp.o"
+  "CMakeFiles/1_StaffManageSystemIndenpently.dir/manager.cpp.o"
+  "CMakeFiles/1_StaffManageSystemIndenpently.dir/worker.cpp.o"
+  "CMakeFiles/1_StaffManageSystemIndenpently.dir/workerManager.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_StaffManageSystemIndenpently.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
